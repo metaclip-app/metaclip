a3f259fd1356f1d70133008bcbb1d51117a5fe2b
Nikollesan <69652684+LuanderFarias@users.noreply.github.com>
2023-11-13 21:50:29 -0300

    Fixing vercel deploy

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/landing.css">
+    <link rel="stylesheet" href="landing.css">
     <title>Metaclip - A canvas for your mind</title>
 </head>
 <body>
